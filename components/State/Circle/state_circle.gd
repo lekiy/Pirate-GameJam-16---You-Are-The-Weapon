9a03767fcf3be92635b9125dc94293506a6a9c21
Lekiy <lekiyminin@gmail.com>
2025-01-20 08:26:35 -0600

    Added some randomness to attacking

--- components/State/Circle/state_circle.gd
+++ components/State/Circle/state_circle.gd
@@ -3,15 +3,17 @@ class_name StateCircle extends State
 @export var body : CharacterBody2D
 @export var move_speed := 200.0
 @export var attack_range := 550.0
+@export var attack_delay_min := 1.0
+@export var attack_delay_max := 2.0
 
-var attack_delay := 1.0
 var wait_time
-
+var strafe_direction
 var target: CharacterBody2D
 
 func state_enter():
 	target = get_tree().get_first_node_in_group("Player")
-	wait_time = attack_delay
+	wait_time = randf_range(attack_delay_min, attack_delay_max)
+	strafe_direction = [1, -1].pick_random()*90
 	
 func state_physics_process(delta: float):
 	wait_time -= delta
@@ -20,7 +22,7 @@ func state_physics_process(delta: float):
 	if wait_time <= 0:
 		transitioned.emit(self, "StateLungeAttack")
 		
-	body.velocity = direction.normalized().rotated(deg_to_rad(90)) * move_speed
+	body.velocity = direction.normalized().rotated(deg_to_rad(strafe_direction)) * move_speed
 		
 	if direction.length() > attack_range:
 		transitioned.emit(self, "StateFollow")
--- enemies/enemy.gd
+++ enemies/enemy.gd
@@ -4,8 +4,8 @@ func _physics_process(delta: float) -> void:
 	move_and_slide()
 	
 	
-	if velocity.x > 0:
+	if velocity.x > 1:
 		$Sprite2D.flip_h = true
-	else:
+	elif velocity.x < 1:
 		$Sprite2D.flip_h = false
 		
